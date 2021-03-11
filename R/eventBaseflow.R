47ae9ac00f1d13157e68ab407a1279aecd0b3098
DanluGuo <danlu.guo@gmail.com>
2021-03-12 10:49:58 +1100

    Update eventBaseflow.R
    Changed baseflow function used in eventBaseflow.R to baseflow (replacing previous baseflow_DG)

--- R/eventBaseflow.R
+++ R/eventBaseflow.R
@@ -40,7 +40,7 @@ eventBaseflow <- function(data, BFI_Th = 0.5, min.diff = 1, threshold = 0, out.s
   # smoothing as of Eqn.3 in https://onlinelibrary.wiley.com/doi/epdf/10.1002/hyp.11185
   # now included in smoothing_DG(...)
 
-  bfi = baseFlow_DG(data)$bfi
+  bfi = baseFlow(data)$bfi
 
   baseind = which(bfi>BFI_Th)
   evind = which(bfi<BFI_Th)
